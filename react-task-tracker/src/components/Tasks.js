71aa8092e3bf7bba86c973ab332dfa1f281d16ab
Yiğit Ömer Mert <yigitomer.merts@gmail.com>
2025-06-24 01:26:11 +0300

    Update Tasks.js

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -3,9 +3,9 @@ import Task from './Task';
 const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
-      {tasks.map((element, index) => (
+      {tasks.map((element) => (
         <Task
-          key={index}
+          key={element.id}
           task={element}
           onDelete={onDelete}
           onToggle={onToggle}
