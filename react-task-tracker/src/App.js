7e8795b27918f6c1c9fe8dff718d9665f3ddde1e
Yiğit Ömer Mert <yigitomer.merts@gmail.com>
2025-06-24 11:43:31 +0300

    Added functionality of deleting tasks from app

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -32,7 +32,10 @@ function App() {
   }
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
     setTasks(tasks.filter((element) => element.id !== id))
   }
 
